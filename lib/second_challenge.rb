2792bda397cad37e807abcaf5009247d0d61fb04
david landau <thelandaus@gmail.com>
2018-05-10 19:33:21 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -5,8 +6,8 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  #code your solution here!
+ # binding.pry
+  groceries.values.flatten
 
   
 
